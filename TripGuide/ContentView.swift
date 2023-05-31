ebe1712ec38180e8ed231bd05810e1b041452de8
KeremCaan <keremcaan@hotmail.com>
2023-05-31 15:23:51 +0300

    Initial Commit

---
+++ TripGuide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TripGuide
+//
+//  Created by MacbookPro on 31.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TripGuide/TripGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripGuideApp.swift
+//  TripGuide
+//
+//  Created by MacbookPro on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TripGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
